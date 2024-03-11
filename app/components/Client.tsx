f954be896bf5c51157f6a9c92959b82a376ddd83
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-11 16:49:23 +0100

    (feat)[Home] client-component update display-animation

--- app/components/Client.tsx
+++ app/components/Client.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import Image from "next/image";
 import logo1 from "./../../public/images/logo.jpg";
 import logo2 from "./../../public/images/logo.jpg";
@@ -20,7 +20,14 @@ const clientContent = {
     description:
       "Je réalise au quotidien des projets pour des startups, PME, éditeurs de logiciels.",
   },
-  logos: [logo1, logo2, logo3, logo4, logo5, logo6],
+  logos: [
+    { logo: logo1, id: 1 },
+    { logo: logo2, id: 2 },
+    { logo: logo3, id: 3 },
+    { logo: logo4, id: 4 },
+    { logo: logo5, id: 5 },
+    { logo: logo6, id: 6 },
+  ],
 };
 
 const Client: React.FC<ClientProps> = ({ className }) => {
@@ -30,14 +37,14 @@ const Client: React.FC<ClientProps> = ({ className }) => {
     offset: ["start end", "end start"],
   });
 
-  const imgScroll1 = useTransform(scrollYProgress, [0, 1], ["20%", "-20%"]);
+  let id = 0;
 
   return (
     <section className={`${className} overflow-hidden`}>
       <div className="container px-4 mx-auto">
         <div className="lg:flex justify-center mb-2">
-          <div className="w-full lg:w-7/12 lg:flex gap-0 items-center mb-20 lg:mb-0 ">
-            <div className="lg:w-7/12">
+          <div className="w-full lg:w-9/12 lg:flex gap-0 items-center mb-20 lg:mb-0 ">
+            <div className="lg:w-8/12">
               {clientContent.intro.subTitle && (
                 <motion.span
                   initial={{ opacity: 0, y: 20 }}
@@ -73,47 +80,38 @@ const Client: React.FC<ClientProps> = ({ className }) => {
                 </motion.h2>
               )}
             </div>
-            <div className="lg:w-5/12 self-end">
-              <motion.p
-                initial={{ opacity: 0, y: 20 }}
+          </div>
+        </div>
+      </div>
+      <div className="w-full lg:w-8/12 items-center justify-between lg:ml-[20%] ml-[5%] mt-20 lg:mb-20">
+        <div className="grid grid-cols-3 md:grid-cols-6">
+          {clientContent.logos.map((logo) => {
+            id += 0.5;
+            return (
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
                 whileInView={{
                   opacity: 1,
                   y: 0,
                   transition: {
-                    delay: 0.15,
-                    duration: 0.5,
+                    delay: id,
+                    duration: 0.2,
                   },
                 }}
                 viewport={{ once: true }}
-                className="text-gray-500"
+                key={logo.id}
+                whileHover={{ y: -10, transition: { duration: 0.01 } }}
+                className="z-[2] relative bg-cover bg-center group duration-300"
               >
-                {clientContent.intro.description}
-              </motion.p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-full lg:w-8/12 items-center justify-between mt-20 lg:mb-20">
-        <div className="grid grid-cols-3 md:grid-cols-6">
-          {clientContent.logos.map((logo, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, x: 20 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  delay: 0.4,
-                  duration: 0.5,
-                },
-              }}
-              viewport={{ once: true }}
-              style={{ y: imgScroll1 }}
-              className="z-[2] relative bg-cover bg-center"
-            >
-              <Image src={logo} width={90} height={90} alt={`logo${index}`} />
-            </motion.div>
-          ))}
+                <Image
+                  src={logo.logo}
+                  width={90}
+                  height={90}
+                  alt={`logo${logo.id}`}
+                />
+              </motion.div>
+            );
+          })}
         </div>
       </div>
     </section>

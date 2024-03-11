04558d5651c83f38ee64d6a818b9457d4dada70a
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-11 23:13:37 +0100

    (feat)[Home] footer component not work  error hydratation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,11 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={inter.className}>
         <Navigation />
         {children}
       </body>
+      <Footer className={""} />
     </html>
   );
 }
